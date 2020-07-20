eecf6ac718dbd83187738b0730de0e7579c799fd
sina chavoshi <chavoshi@google.com>
2020-07-20 11:02:16 -0700

    clean up formatting

--- tests/integration/e2e_call_run_on_script_with_keras_test.py
+++ tests/integration/e2e_call_run_on_script_with_keras_test.py
@@ -26,7 +26,8 @@ REMOTE_DIR = os.environ["E2E_TEST_BUCKET"]
 BUILD_ID = os.environ["E2E_BUID_ID"]
 
 # Path to the source code in test enviroment
-TEST_DATA_PATH = os.path.dirname(os.path.abspath(__file__))
+TEST_DATA_PATH = os.path.join(
+  os.path.dirname(os.path.abspath(__file__)), '../testdata/')
 
 class TensorflowCloudOnScriptTest(tf.test.TestCase):
 
@@ -52,7 +53,7 @@ class TensorflowCloudOnScriptTest(tf.test.TestCase):
     with self.assertRaises(RuntimeError):
       tfc.run(
           entry_point=os.path.join(
-            TEST_DATA_PATH, '../testdata/mnist_example_using_fit_no_reqs.py'),
+            TEST_DATA_PATH, 'mnist_example_using_fit_no_reqs.py'),
           entry_point_args=["--path", self.save_model_path],
       )
     mock_exit.assert_called_once_with(0)
