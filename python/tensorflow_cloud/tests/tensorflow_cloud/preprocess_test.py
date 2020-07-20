698594dda686b56c92cacc00c7642d69438640c2
sina chavoshi <chavoshi@google.com>
2020-07-20 13:14:36 -0700

    update unittest path

--- python/tensorflow_cloud/tests/tensorflow_cloud/preprocess_test.py
+++ python/tensorflow_cloud/tests/tensorflow_cloud/preprocess_test.py
@@ -22,11 +22,11 @@ from tensorflow_cloud import preprocess
 
 class TestPreprocess(unittest.TestCase):
     def setup_py(self):
-        self.entry_point = "testdata/sample_compile_fit.py"
+        self.entry_point = "python/tensorflow_cloud/tests/testdata/sample_compile_fit.py"
         _, self.entry_point_name = os.path.split(self.entry_point)
 
     def setup_ipython(self):
-        self.entry_point = "tests/testdata/mnist_example_using_fit.ipynb"
+        self.entry_point = "python/tensorflow_cloud/tests/testdata/mnist_example_using_fit.ipynb"
         _, self.entry_point_name = os.path.split(self.entry_point)
 
     def get_preprocessed_entry_point(
