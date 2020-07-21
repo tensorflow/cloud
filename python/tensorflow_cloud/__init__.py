6f3eadae6cc49dc8682a29e86e6a00dbe7795b6b
sina chavoshi <chavoshi@google.com>
2020-07-20 17:13:40 -0700

    Add version back to __init__.py

--- python/tensorflow_cloud/__init__.py
+++ python/tensorflow_cloud/__init__.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+__version__ = "0.1.4"
+
 from .core.machine_config import AcceleratorType
 from .core.machine_config import COMMON_MACHINE_CONFIGS
 from .core.machine_config import MachineConfig
