30da4a5b65e1dd6c7e9d0d3426a8f1bd3564ee46
sina chavoshi <chavoshi@google.com>
2020-08-14 12:46:51 -0700

    add comment

--- src/python/tensorflow_cloud/tuner/optimizer_client.py
+++ src/python/tensorflow_cloud/tuner/optimizer_client.py
@@ -26,7 +26,10 @@ from googleapiclient import http as googleapiclient_http
 import tensorflow as tf
 from tensorflow_cloud import version
 
-#  PLACE_HOLDER_FOR_TEST_IMPORT_DO_NOT_DELETE
+# Following placeholder tag is for copybara rule that replace the tag
+# With an import statement of an internal library. The Library is needed
+# to pass file path in build infrastructure for API documentation file.
+# PLACE_HOLDER_FOR_TEST_IMPORT_DO_NOT_DELETE
 
 _OPTIMIZER_API_DOCUMENT_FILE = "api/ml_public_google_rest_v1.json"
 
